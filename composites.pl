cb03da0649549608153da1092e169cb0c99779d9
fnl <florian.leitner@gmail.com>
2013-11-28 18:41:11 +0100

    extract composite (hyphenated, underscored) tokens

---
+++ composites.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/env perl
+
+# this script extracts token variants of type "T1_T2" and "T1-T2"
+
+# Auhtor: Florian Leitner <florian.leitner@gmail.com>
+# (C) 2013. All rights reserved.
+# License: Apache License v2 <https://www.apache.org/licenses/LICENSE-2.0.html>
+
+use File::Basename;
+my $prog = basename($0);
+if ($#ARGV == 0 && $ARGV[0] =~/-h|--help/) {
+  print STDERR "usage: $prog < TOKENS > TOKENS\n";
+  exit 1;
+}
+
+use open ':locale';
+
+while (<>) {
+  if (/[_-]/) {
+    @words = split;
+    foreach (@words) {
+      print "$_\n" if (/[\p{L}\p{N}][_-][\p{L}\p{N}]/); 
+    }
+  }
+}
